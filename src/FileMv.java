1b4b2283c3829a29b0e0bf4d4fa848a8fb5a7dec
washrotom <wash_rotom@naver.com>
2020-07-08 02:11:07 +0900

    first push

---
+++ src/FileMv.java
@@ -0,0 +1,18 @@
+import java.io.File;
+
+//Files.move(soure, target, option);
+//이식을 목적으로 만들기 때문에 메인에서는 인수를 받기만 한다.
+public class FileMv {
+
+	public static void main(String[] args) {
+
+	}
+	//파일의 최근수정날짜를 읽는다
+	//일치하는 폴더가 없다 -> 그 날짜로 만든다 -> 스트링 뒤에 그 날짜의 데이터를 넣는다.
+	public void Move(String targetDir) {
+		String TargetDir = targetDir;
+		
+	}
+
+}
+
